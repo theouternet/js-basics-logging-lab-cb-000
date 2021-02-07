5a50f03223ed0d24b4ccfdb86ce9ff5d55667e79
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2021-02-07 19:05:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const driver = 'bob';
+let driver = bob;
 
-console.log ('this code was called')
-
-console.log (driver)
-
-console.log (driver, 'is the driver variable name')
-
+function describe {
+  
+}
